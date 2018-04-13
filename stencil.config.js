0af4823e0f788775b4492ad8be210cc0ed882c0a
Nicolas Thomas <n.thomas@waigeo.fr>
2018-04-13 14:27:13 +0200

    Fri Apr 13 14:27:13     2018

--- stencil.config.js
+++ stencil.config.js
@@ -1,5 +1,19 @@
 exports.config = {
+  //namespace: 'app',
+  outputTargets: [
+    { type: 'dist' },
+    { type: 'www' }
+  ]
 };
+/*
+exports.config = {
+  namespace: 'dynacare-pro',
+  outputTargets: [
+    { type: 'dist' },
+    { type: 'www' }
+  ]
+};
+*/
 
 exports.devServer = {
   root: 'www',
